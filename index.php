0f8bdff4331e6b910f573e0eda01120d5e963d00
linkvetern64 <jstand1@umbc.edu>
2016-03-25 14:51:29 -0400

    Added files

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php
+
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Class Selector</title>
+</head>
+<body>
+<form method="POST" action="classSelect.php">
+Classes: <input type="text" name="taken" /><br />
+<input type="submit" name="submit" />
+</form>
+</body>
+</html>
