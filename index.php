3d79fd136a48ab8eea1a9c2e9da5cfe49fbbe915
linkvetern64 <jstand1@umbc.edu>
2016-04-05 00:27:43 -0400

    Added styling

--- index.php
+++ index.php
@@ -16,20 +16,21 @@
 	</style>
 </head>
 <body class="bg">
-	<div class="background-image"></div>
 	<div id="container">
-	<br />
- 	<div id="header"></div>
- 	<div id="body">
-	<form method="POST" action="validate.php">
-	<input type="text" name="firstName" class="top" placeholder="First Name"/>	<?php if($_SESSION["validFN"]){echo("<span style='color:red;'>*Only #'s, or not empty</span>");} ?><br />
-	<input type="text" name="lastName" placeholder="Last Name" class="middle"/>	<?php if($_SESSION["validLN"]){echo("<span style='color:red;'>*Only #'s, or not empty</span>");} ?><br />
-	<input type="text" name="umbcID" placeholder="UMBC ID" class="middle"/>		<?php if($_SESSION["validID"]){echo("<span style='color:red;'>*Only #'s, or not empty</span>");} ?><br />
-	<input type="text" name="email" placeholder="Email" class="bottom"/>		<?php if($_SESSION["validE"]){echo("<span style='color:red;'>*Only #'s, or not empty</span>");} ?><br />
-	<button class="button"  style="vertical-align:middle"><span>Submit &nbsp;</span></button>
-	</form>
+		<br />
+	 	<div id="header"></div>
+
+	 	<div id="body">
+			<form method="POST" action="validate.php">
+				<input type="text" name="firstName" class="top" placeholder="First Name"/>	<?php if($_SESSION["validFN"]){echo("<span style='color:red;'>*Only #'s, or not empty</span>");} ?><br />
+				<input type="text" name="lastName" placeholder="Last Name" class="middle"/>	<?php if($_SESSION["validLN"]){echo("<span style='color:red;'>*Only #'s, or not empty</span>");} ?><br />
+				<input type="text" name="umbcID" placeholder="UMBC ID" class="middle"/>		<?php if($_SESSION["validID"]){echo("<span style='color:red;'>*Only #'s, or not empty</span>");} ?><br />
+				<input type="text" name="email" placeholder="Email" class="bottom"/>		<?php if($_SESSION["validE"]){echo("<span style='color:red;'>*Only #'s, or not empty</span>");} ?><br />
+				<button class="button"  style="vertical-align:middle"><span>Submit &nbsp;</span></button>
+			</form>
+		</div>
 	</div>
-	</div>
+	<div id="footer"></div>
 	<!-- TEST STUFF -->
 
 	<!-- END TEST STUFF-->
--- validate.php
+++ validate.php
@@ -6,24 +6,24 @@
 	$_SESSION["validE"] = "";
 	$_SESSION["validID"] = "";
 
-	if(preg_match("/[a-zA-Z]+/",$_POST["firstName"])){
+	if(!preg_match("/[a-zA-Z]+/",$_POST["firstName"])){
 		$_SESSION["validFN"] = true;
 	}
 	else{
 		$_SESSION["validFN"] = false;
 	}
-	if(preg_match("/[a-zA-Z]+/",$_POST["lastName"])){
+	if(!preg_match("/[a-zA-Z]+/",$_POST["lastName"])){
 		$_SESSION["validLN"] = true;
 	}
 	else{
 		$_SESSION["validLN"] = false;
 	}
-	if(preg_match("/.+@.+\..+/",$_POST["email"])) {
+	if(!preg_match("/.+@.+\..+/",$_POST["email"])) {
 	  $_SESSION["validE"] = true;
 	} else {
 	  $_SESSION["validE"] = false;
 	}
-	if(preg_match("/[a-zA-Z]+/",$_POST["umbcID"])){
+	if(!preg_match("/[a-zA-Z]+/",$_POST["umbcID"])){
 		$_SESSION["validID"] = true;
 	}
 	else{
